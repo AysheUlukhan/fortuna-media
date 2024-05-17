816ed72481f2232a9574c3247b6ff7a059f39e11
AyseUluxan <ayshe.ulukhhanova@gmail.com>
2024-05-18 03:37:43 +0400

    abdula push

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../assets/css/services.css';
 import service_card_1 from '../assets/images/service-page-img/service-card-1.png';
 import service_card_2 from '../assets/images/service-page-img/service-card-2.png';
@@ -6,8 +6,13 @@ import service_card_3 from '../assets/images/service-page-img/service-card-3.png
 import service_card_4 from '../assets/images/service-page-img/service-card-4.png';
 import service_card_5 from '../assets/images/service-page-img/service-card-5.png';
 import service_card_6 from '../assets/images/service-page-img/service-card-6.png';
+import { ApiContext } from '../context/ApiContext';
 
 const Services = () => {
+
+  const {services} = useContext(ApiContext);
+  console.log(services);
+
   return (
     <div className='fm-service'>
       <section className='fm-service-page mb-5 py-5'>
@@ -24,20 +29,28 @@ const Services = () => {
 
       <section className='fm-service-card'>
         <div className="container px-4 py-5">
-          <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div className="col-12 col-sm-12 col-lg-6 col-md-12">
-              <img src={service_card_1} className="d-block mx-lg-auto img-fluid w-100" alt="Bootstrap Themes" />
-            </div>
-            <div className="col-lg-6 col-12 col-sm-12 col-md-12">
-              <h5 className='pb-4'>Dizayn</h5>
-              <p>Bizim dizayn prosesimiz, əməkdaşlıq və tökmə ilə köklənmişdir və biz sizin vizyonunuzu əhatə edərək və gücləndirərək təmin edirik. İlk konseptlərdən son təhvilatına qədər, vizual olaraq gözəl və funksional dizaynlara nail olmağa olan əməyimizlə sizi rəzdən çıxaran dizaynlara nail olmağa ümid edirik. İxtisasımız, inkişafımız və müştəri-mərkəzli yanaşmamız üçün FortunaMedia-nı seçin - birgə brendinizi yüksəldək!</p>
-              <div className="d-grid gap-2 justify-content-sm-center justify-content-lg-end">
-                <a href='#'>Daha çox oxu</a>
-              </div>
-            </div>
+        
+          <div className="row align-items-center g-5 py-5">
+            {
+              services.map((item, index) => (
+                <div key={index} className={`section ${index % 2 === 1 ? 'left' : 'right'}`}>
+                  <div className="col-12 col-sm-12 col-lg-6 col-md-12">
+                    <img src={item.image} className="d-block mx-lg-auto img-fluid w-100" alt="Bootstrap Themes" />
+                  </div>
+                  <div className="col-lg-6 col-12 col-sm-12 col-md-12">
+                    <h5 className='pb-4'>{item.service_title}</h5>
+                    <p>{item.description}</p>
+                    <div className="d-grid gap-2 justify-content-sm-center justify-content-lg-end">
+                      <a href='#'>Daha çox oxu</a>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+
           </div>
 
-          <div className="row  align-items-center g-5 py-5">
+          {/* <div className="row  align-items-center g-5 py-5">
             <div className="col-12 col-sm-12 col-lg-6 col-md-12">
               <img src={service_card_2} className="d-block mx-lg-auto img-fluid w-100" alt="Bootstrap Themes" />
             </div>
@@ -100,7 +113,7 @@ const Services = () => {
                 <a href='#'>Daha çox oxu</a>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </section>
     </div>
